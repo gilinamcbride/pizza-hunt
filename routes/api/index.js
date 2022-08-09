877cfd6ad5a7dc4e6e5aa934fed8cbfb3cadf808
Gilina McBride <gilinamcbride@gmail.com>
2022-08-09 12:43:03 -0400

    created comment model, routes, controllers

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const pizzaRoutes = require("./pizza-routes");
+const commentRoutes = require("./comment-routes");
 
-// add prefix of `/pizzas` to routes created in `pizza-routes.js`
+// add prefix of `/name` to routes created in `name-routes.js`
 router.use("/pizzas", pizzaRoutes);
+router.use("/comments", commentRoutes);
 
 module.exports = router;
